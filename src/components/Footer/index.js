b93e27b8471061c1a84bfd7ed64fa229103542cc
Mr. A <ag.perch@gmail.com>
2020-08-26 16:33:07 -0700

    working on the carousel

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./style.css"
+export default function Footer() {
+    return (
+        <footer className="foot mt-3">
+            <p className="text-center">Footer</p>
+        </footer>
+    )
+}
--- src/components/Row1/index.js
+++ src/components/Row1/index.js
@@ -5,7 +5,7 @@ import Video from "../Video"
 export default function row1() {
 
     return (
-        <div className="row top pt-4 d-flex justify-content-center">
+        <div className="row top pt-4 ">
             <div className="col-12">
             <Video/>
             </div>
