30f18b4602854cabf18f0319a5180651aefbe9e3
Mr. A <ag.perch@gmail.com>
2020-08-26 18:29:00 -0700

    added react player

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,23 +1,27 @@
 import React,{useState,useEffect} from 'react'
 import Images from "../../Util/ImageUrl/ImageUrl.json"
+import StartText from '../StartText'
 
 export default function Carousel() {
     const[imageArray,setImageArray]=useState(Images)
     const[counter,setCounter]=useState(0)
 
-    
-        // let newCounter = counter
-        // let myInterval = setInterval(() => {
-        //     if(counter >= imageArray.length -1){
-        //         setCounter(0)
+    useEffect(()=>{
+
+        let newCounter = counter
+        let myInterval = setTimeout(() => {
+            if(counter >= imageArray.length -1){
+                setCounter(0)
               
-        //     }else{
-        //         newCounter = counter +1
-        //         setCounter(newCounter)
-        //     }
-        //     console.log(1);
+            }else{
+                newCounter = counter +1
+                setCounter(newCounter)
+            }
+            console.log(1);
             
-        // }, 3000);
+        }, 3000);
+    },[counter])
+        
 
     
     return (
